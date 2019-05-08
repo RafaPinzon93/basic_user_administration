7cc56b11068df85537a7446a105badd69a68fc4c
RafaPinzon93 <rafapinzon_@hotmail.com>
2019-05-08 16:04:15 -0500

    Added edit user, Basic

--- administration/urls.py
+++ administration/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import login_view, UserListView, create_user
+from .views import login_view, UserListView, create_user, UserUpdate
 
 urlpatterns = [
     path('', login_view),
-    path('users', UserListView.as_view(), name='user_list'),
-    path('users/create', create_user, name='user_create'),
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/create/', create_user, name='user_create'),
+    path('users/edit/<int:pk>', UserUpdate.as_view(), name='user_edit'),
 ]
--- administration/views.py
+++ administration/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -34,4 +36,18 @@ def create_user(request):
             return redirect('user_list')
     else:
         form = UserForm()
-    return render(request, 'administration/create_user.html', {'form': form})
+    return render(request, 'administration/user_form.html', {'form': form, 'title': "Create User"})
+
+
+class UserUpdate(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = "administration/user_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Update User"
+        return context
+
+    def get_success_url(self):
+        return reverse('user_list')
