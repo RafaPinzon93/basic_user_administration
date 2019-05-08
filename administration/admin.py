24df8d52ee5b4de42761c6011e7a559c5c6da743
RafaPinzon93 <rafapinzon_@hotmail.com>
2019-05-07 19:55:41 -0500

    Added fields IBAN and created_by to User.

--- administration/admin.py
+++ administration/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from .models import User
 
-# Register your models here.
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserAdmin(UserAdmin):
+    ADDITIONAL_USER_FIELDS = (
+        (
+            None, {'fields': ('iban',)}
+        ),
+    )
+
+    model = User
+    form = MyUserChangeForm
+    fieldsets = UserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
+
+
+admin.site.register(User, CustomUserAdmin)
--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    iban = models.CharField(max_length=34)
+    created_by = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name="users", blank=True, null=True)
